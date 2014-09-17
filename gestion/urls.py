701fe6eaef0da99ab8d55d2797eda7cd2022cfa1
Jon Latorre <moebius@etxea.net>
2014-09-17 22:23:32 +0200

    mejoras varias

--- gestion/urls.py
+++ gestion/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 
 from django.views.generic import TemplateView
 from django.views.generic import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from videowm.models import *
 from views import *
@@ -14,5 +14,6 @@ urlpatterns = patterns("",
     url(r"^$", WebminarGestionList.as_view(), name="webminar_gestion_lista"),
     url(r"^nuevo/$", CreateView.as_view(model=Webminar), name="webminar_nuevo"),
     url(r"^editar/(?P<pk>\d+)/$", UpdateView.as_view(model=Webminar), name="webminar_editar"),
+    url(r"^borrar/(?P<pk>\d+)/$", DeleteView.as_view(model=Webminar,success_url="/gestion/"), name="webminar_borrar"),
     url(r"^realizar/(?P<pk>\d+)/$", WebminarRealizar.as_view(), name="webminar_realizar"),
     )
--- webminar/urls.py
+++ webminar/urls.py
@@ -1,18 +1,18 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
+from django.core.urlresolvers import reverse_lazy
 
 from django.contrib import admin
 
 
 urlpatterns = patterns("",
-    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
+    url(r"^$", RedirectView.as_view(url=reverse_lazy('webminar_portada')), name="home"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
     url(r"^webminar/", include("videowm.urls")),
     url(r"^gestion/", include("gestion.urls")),
-    
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
