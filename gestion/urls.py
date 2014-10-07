448b04f9b403ef42c52e8d081dadc6f67d07141f
Jon Latorre <info@etxea.net>
2014-10-07 10:43:08 +0200

    Ajustes, sobre todo login para ver la parte de gestion

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
 
 from django.views.generic import TemplateView
 from django.views.generic import ListView
@@ -12,9 +13,9 @@ from views import *
 
 
 urlpatterns = patterns("",
-    url(r"^$", WebminarGestionList.as_view(), name="webminar_gestion_lista"),
-    url(r"^nuevo/$", CreateView.as_view(model=Webminar,form_class=WebminarForm), name="webminar_nuevo"),
-    url(r"^editar/(?P<pk>\d+)/$", UpdateView.as_view(model=Webminar), name="webminar_editar"),
-    url(r"^borrar/(?P<pk>\d+)/$", DeleteView.as_view(model=Webminar,success_url="/gestion/"), name="webminar_borrar"),
-    url(r"^realizar/(?P<pk>\d+)/$", WebminarRealizar.as_view(), name="webminar_realizar"),
+    url(r"^$", login_required(WebminarGestionList.as_view()), name="webminar_gestion_lista"),
+    url(r"^nuevo/$", login_required(CreateView.as_view(model=Webminar,form_class=WebminarForm)), name="webminar_nuevo"),
+    url(r"^editar/(?P<pk>\d+)/$", login_required(UpdateView.as_view(model=Webminar)), name="webminar_editar"),
+    url(r"^borrar/(?P<pk>\d+)/$", login_required(DeleteView.as_view(model=Webminar,success_url="/gestion/")), name="webminar_borrar"),
+    url(r"^realizar/(?P<pk>\d+)/$", login_required(WebminarRealizar.as_view()), name="webminar_realizar"),
     )
--- gestion/views.py
+++ gestion/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic import ListView,DetailView
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.decorators import login_required, permission_required
 
 from videowm.models import *
 
