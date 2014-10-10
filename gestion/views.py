0560a4d023c5ca832ccd8acbc13a57389dce287a
Jon Latorre <jonlatorremartinez@gmail.com>
2014-10-10 15:01:01 +0200

    Expotar a CSV: utf8 y ; como separador

--- gestion/views.py
+++ gestion/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from django.views.generic import ListView,DetailView
@@ -23,19 +25,20 @@ def exportar_mensajes(request,pk):
     webminar = get_object_or_404(Webminar, pk=pk)
     # Create the HttpResponse object with the appropriate CSV header.
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
-    writer = csv.writer(response)
+    response['Content-Disposition'] = 'attachment; filename="mensajes_%s.csv"'%webminar.slug
+    writer = csv.writer(response,delimiter=';')
     writer.writerow(['Fecha', 'De', 'Para', 'Texto'])
     for mensaje in webminar.mensaje_set.all().order_by('-fecha'):
-        writer.writerow([mensaje.fecha, mensaje.de, mensaje.para, mensaje.texto])
+		print mensaje.fecha, mensaje.de, mensaje.para, mensaje.texto
+		writer.writerow([mensaje.fecha, mensaje.de, mensaje.para, mensaje.texto.encode("utf-8")])
     return response
 
 def exportar_visitas(request,pk):
     webminar = get_object_or_404(Webminar, pk=pk)
     # Create the HttpResponse object with the appropriate CSV header.
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
-    writer = csv.writer(response)
+    response['Content-Disposition'] = 'attachment; filename="visitas_%s.csv"'%webminar.slug
+    writer = csv.writer(response,delimiter=';')
     writer.writerow(['Fecha', 'Quien'])
     for mensaje in webminar.visita_set.all().order_by('-fecha'):
         writer.writerow([mensaje.fecha, mensaje.quien])
